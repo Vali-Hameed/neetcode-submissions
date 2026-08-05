46df647fb0b260d2009b8235afeb5adc765c225e
Vali Hameed <138222890+Vali-Hameed@users.noreply.github.com>
2026-08-05 18:33:16 +0100

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pos_sp_map={}
+        stack=[]
+        for i in range (len(position)):
+            pos_sp_map[position[i]]=speed[i]
+        position = sorted(position,reverse = True)
+        fleet = len(position)
+        for i in range(len(position)):
+
+            cur_speed=pos_sp_map[position[i]]
+            time = (target-position[i])/cur_speed
+            if len(stack)>0:
+                if time>stack[-1]:
+                    stack.append(time)
+
+            if i == 0:
+                stack.append(time)
+         
+        return len(stack)
